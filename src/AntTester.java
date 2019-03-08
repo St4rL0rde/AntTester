36cb847acaf4b898b88ebf0931e15ff7e457e8c3
Batavis <38907830+Batavis@users.noreply.github.com>
2019-03-08 11:42:46 +0100

    Set up random gamble

--- src/AntTester.java
+++ src/AntTester.java
@@ -1,8 +1,13 @@
+import static java.lang.Math.sqrt;
+import static jdk.nashorn.internal.objects.NativeMath.random;
 
 public class AntTester {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		int k = (int) random(100);
+		for (int i = 0; i<sqrt(k);i++) {
+			if (i<5) System.out.println("<=25");
+		}
 	}
 
 }

aa84c8bc60c0361ca5a7ca9d1d677fe24b881355
St4rL0rde <48772684+St4rL0rde@users.noreply.github.com>
2019-03-20 21:58:38 +0100

    Update AntTester.java

--- src/AntTester.java
+++ src/AntTester.java
@@ -4,9 +4,9 @@ import static jdk.nashorn.internal.objects.NativeMath.random;
 public class AntTester {
 
 	public static void main(String[] args) {
-		int k = (int) random(100);
-		for (int i = 0; i<sqrt(k);i++) {
-			if (i<5) System.out.println("<=25");
+		int kr = (int) random(100);
+		for (int i = 10 i<sqrt(kr);i--) {
+			if (i<5) System.out.println("<25");
 		}
 	}
 

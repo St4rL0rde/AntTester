7806b612ebb271375eea578e913ec1d6952e0773
St4rL0rde <48772684+St4rL0rde@users.noreply.github.com>
2019-03-20 21:32:02 +0100

    Update AntTester.java

--- src/AntTester.java
+++ src/AntTester.java
@@ -5,9 +5,13 @@ public class AntTester {
 
 	public static void main(String[] args) {
 		int k = (int) random(100);
-		for (int i = 0; i<sqrt(k);i++) {
-			if (i<5) System.out.println("<=25");
-		}
+		int j = 0;
+		while (j<sqrt(k)) {
+			if (j<5) Sytem.out.println("<=25");
+		{
+		// for (int i = 0; i<sqrt(k);i++) {
+		// 	if (i<5) System.out.println("<=25");
+		// }
 	}
 
 }
